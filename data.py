e7f0576dd3c081817fc557acfa461ce4e2f8e646
JackChen0 <stu30305@gmail.com>
2019-01-01 23:11:59 +0800

    Add filiter

--- data.py
+++ data.py
@@ -7,3 +7,10 @@ with open('reviews.txt', 'r') as r:
 print('Here are ',len(data),'commits.')
 print('The total length is',sum_len)
 print('Averange length of data is', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('There are', len(new),'commits shortert than 100 bytes.')
+print(new[0])
