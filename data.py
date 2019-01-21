68cce71fa5821e197895be5c74375e71468c79a8
JackChen0 <stu30305@gmail.com>
2019-01-21 08:58:40 +0800

    Add counter

--- data.py
+++ data.py
@@ -6,11 +6,45 @@ with open('reviews.txt', 'r') as r:
 		data.append(line)
 print('Here are ',len(data),'commits.')
 print('The total length is',sum_len)
-print('Averange length of data is', sum_len / len(data))
+# print(data[0])
+# print('Averange length of data is', sum_len / len(data))
 
-new = []
+wc = {}
 for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('There are', len(new),'commits shortert than 100 bytes.')
-print(new[0])
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1 #查找並增加
+		else:
+			wc[word] = 1 #新增KEY
+# for word in wc:
+# 	if wc[word] > 1000:
+# 		print(word, wc[word])
+
+while  True:
+	word = input('Please enter the keyword ')
+	if word == 'q':
+		print('Thank you')
+		break
+	if word in wc:
+		print('We use ', word,wc[word],' times')
+	else:
+		print('We can not find the word')
+	
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('There are', len(new),'commits shortert than 100 bytes.')
+# print(new[0])
+
+# keyword = []
+# for d in data:
+# 	if 'good' in d:
+# 		if 'great' in d:
+# 			keyword.append(d)
+# print('There are', len(keyword),'commits mentioned the word"good".')
+# print(keyword[0])
+
+# keyword = [d for d in data if 'good' in d]
+# print(len(keyword)[0])
